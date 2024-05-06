0d343f737dbea288217655ebf4d76aa1d9fb032d
Ad Kaium <adkaiumalikhan01@gmail.com>
2024-05-06 07:14:23 +0300

    create user delete api

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require('express');
+const express = require("express");
 const {MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-
 
 // middlewars
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster01.rrfocni.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -32,59 +28,64 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const userCollection = client.db('bistroDb').collection("users");
-    const menuCollection = client.db('bistroDb').collection("menu");
-    const reviewCollection = client.db('bistroDb').collection("reviews");
-    const cartCollection = client.db('bistroDb').collection("carts");
+    const userCollection = client.db("bistroDb").collection("users");
+    const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
 
-  // user api
-  app.post('/users', async(req,res)=>{
-    const user = req.body;
-    // insert email if user doesnt exists:
-    // you can do this many ways (1. email uniqe, 2.upsert, 3.simple chaecking)
-    const query ={email: user.email}
-    const existingUser = await userCollection.findOne(query)
-    if(existingUser){
-      return res.send({message:'user already Exist', insertedId: null})
-    }
-    const result = await userCollection.insertOne(user)
-    res.send(result)
-  })
+    // user api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      // insert email if user doesnt exists:
+      // you can do this many ways (1. email uniqe, 2.upsert, 3.simple chaecking)
+      const query = {email: user.email};
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({message: "user already Exist", insertedId: null});
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
+    // itme api
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/review", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
 
-// itme api
-    app.get('/menu', async(req,res)=>{
-        const result = await menuCollection.find().toArray();
-        res.send(result);
-    })
-    app.get('/review', async(req,res)=>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
-    })
-
-
- // cart api
-   app.get('/carts',async(req,res)=>{
-     const email= req.query.email;
-     const query ={email:email}
-     const result = await cartCollection.find(query).toArray();
-     res.send(result)
-   })
-   app.post('/carts', async(req,res)=>{
+    // cart api
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
-      res.send(result)
-   })
-   app.delete('/carts/:id', async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await cartCollection.deleteOne(query);
-    res.send(result)
-   })
-
-
-
+      res.send(result);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ping: 1});
@@ -98,11 +99,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("boss is Running");
+});
 
-app.get('/', (req,res)=>{
-    res.send('boss is Running')
-})
-
-app.listen(port, ()=>{
-    console.log(`Bistro boss is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Bistro boss is running on port ${port}`);
+});
